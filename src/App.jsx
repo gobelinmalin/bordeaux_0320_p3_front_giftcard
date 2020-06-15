bf29864a7f2051bc303ab4dc045a7cbc711c4e54
PierreVelluet <velluetp@gmail.com>
2020-06-15 19:08:35 +0200

    job done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contact from './components/Contact/Contact';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello React World</header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/contact" component={Contact} exact />
+      </Switch>
+    </Router>
   );
 }
 
