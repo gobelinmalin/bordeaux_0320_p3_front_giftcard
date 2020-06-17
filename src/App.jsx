c8f3815d4958e362704fbce23491fb0fd0c23200
Chris-css <christelle.savoca@gmail.com>
2020-06-17 16:15:53 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello React World</header>
+      <Header />
     </div>
   );
 }
