c696c19f18c6dbd8860a0922d71f68f509a961e0
PierreVelluet <velluetp@gmail.com>
2020-06-18 10:20:09 +0200

    job done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Contact from './components/Contact/Contact';
