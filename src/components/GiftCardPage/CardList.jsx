5e8d4ad1cd29f56ea9c952d372dae4887e6753ed
PierreVelluet <velluetp@gmail.com>
2020-07-02 11:59:47 +0200

    job done

--- src/components/GiftCardPage/CardList.jsx
+++ src/components/GiftCardPage/CardList.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './CardList.css';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import CardProduct from '../Home/CardProduct';
+import * as actionCreators from '../../actions/index';
 
 const CardList = (props) => {
-  const { state } = props;
+  const { state, setFinalArray } = props;
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_LOCALHOST}/api/products`)
+      .then((res) => setFinalArray(res.data));
+  }, []);
+
   return (
-    <div className="FinalFilter">
-      {state.finalArray.map((element) => (
-        <CardProduct key={element.id} product={element} />
-      ))}
+    <div>
+      {state.finalArray.length > 0 ? (
+        <div className="FinalFilter">
+          {state.finalArray.map((element) => (
+            <CardProduct key={element.id} product={element} />
+          ))}
+        </div>
+      ) : (
+        <p className="FinalFilter">
+          Aucune carte correspondante à votre recherche
+        </p>
+      )}
     </div>
   );
 };
 
 CardList.propTypes = {
+  setFinalArray: PropTypes.func,
   state: PropTypes.arrayOf(
     PropTypes.oneOfType([PropTypes.number, PropTypes.string])
   ),
@@ -23,6 +41,7 @@ CardList.propTypes = {
 
 CardList.defaultProps = {
   state: [],
+  setFinalArray: '',
 };
 
 const mapStateToProps = (state) => {
@@ -31,4 +50,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(CardList);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setFinalArray: (array) => dispatch(actionCreators.setFinalArray(array)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardList);
